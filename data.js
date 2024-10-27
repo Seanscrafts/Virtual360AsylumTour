0d77d28158f6b79041d3e787253e3bf124d5acc9
Seanscrafts <60417231+Seanscrafts@users.noreply.github.com>
2024-10-27 16:41:36 -0700

    Added all project files for 360 Asylum Tour

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cinecameraactor364",
+      "name": "CineCameraActor364",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0837173904081663,
+          "pitch": 0.03619171543390998,
+          "rotation": 7.853981633974483,
+          "target": "1-cinecameraactor360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cinecameraactor360",
+      "name": "CineCameraActor360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.5920160845547429,
+        "pitch": 0.23122035578906974,
+        "fov": 1.442750951239218
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7598870112295284,
+          "pitch": 0.03232049839550477,
+          "rotation": 1.5707963267948966,
+          "target": "2-cinecameraactor361"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2885165571918105,
+          "pitch": -0.2051312328143613,
+          "title": "Window too high",
+          "text": "The \"Window Too High\" sculpture at Victoria Hill in New Westminster reflects the isolation and lack of control experienced by residents of the former Woodlands Institution. It symbolizes the physical and emotional barriers faced in institutional care."
+        }
+      ]
+    },
+    {
+      "id": "2-cinecameraactor361",
+      "name": "CineCameraActor361",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3753932578062731,
+          "pitch": 0.13453623457606056,
+          "rotation": 1.5707963267948966,
+          "target": "4-cinecameraactor363"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cinecameraactor362",
+      "name": "CineCameraActor362",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-cinecameraactor363",
+      "name": "CineCameraActor363",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7067827659912034,
+          "pitch": 0.06947530697932613,
+          "rotation": 1.5707963267948966,
+          "target": "1-cinecameraactor360"
+        },
+        {
+          "yaw": -0.6069606063649466,
+          "pitch": 0.10356670196093276,
+          "rotation": 4.71238898038469,
+          "target": "0-cinecameraactor364"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
